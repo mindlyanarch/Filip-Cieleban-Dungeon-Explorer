bbaefed8b53f5fe5c75b9d4dc17b6b391df70e48
mindlyanarch <29086791@students.lincoln.ac.uk>
2025-03-05 15:57:40 +0000

    Updated Player.Look
    -Player.Look now correctly identifies room adjacency

--- Player.cs
+++ Player.cs
@@ -55,17 +55,25 @@ namespace DungeonExplorer
 
                 //check if room has adjacency
 
-                 int Coords = Map[currentRoom];
 
+                int Coords = Map[currentRoom];
+                Dictionary<string, int> Cardinality = new();
 
-                if (Map.Values.Contains(Coords + 10))
-                {
-                    Console.WriteLine("There is a room to your right");
-                }
-                else
-                {
-                    Console.WriteLine("There is nothing to your right");
-                }
+                Cardinality.Add("North", 1);
+                Cardinality.Add("East", 10);
+                Cardinality.Add("South", -1);
+                Cardinality.Add("West", -10);
+
+                for (int i = 0; i < 4; i++)
+
+                    if (Map.Values.Contains(Coords + Cardinality.Values.ElementAt(i)))
+                    {
+                        Console.WriteLine("There is a room to your {0}", Cardinality.Keys.ElementAt(i));
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is nothing to your {0}", Cardinality.Keys.ElementAt(i));
+                    }
 
             }
 
